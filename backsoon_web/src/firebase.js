cac78df524aed12f27c4912b3a9fd2dd15dd1c99
Vivek Fauzdar <58414270+vivekmfauzdar@users.noreply.github.com>
2023-11-01 23:24:04 +0530

    Update firebase.js

--- backsoon_web/src/firebase.js
+++ backsoon_web/src/firebase.js
@@ -1,13 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import {getStorage} from 'firebase/storage'
-import { getDatabase} from 'firebase/database'
+
  
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,11 +9,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const dataref = firebase.firestore();
-const storage = getStorage(firebaseApp)
-const dbref = getDatabase(firebaseApp)
 
-export { firebaseApp, auth, dataref, storage, dbref };
 
